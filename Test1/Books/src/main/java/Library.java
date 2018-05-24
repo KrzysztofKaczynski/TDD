e31e85672c4d2f2ee1b81ce264a6039b56a83e43
KrzysztofKaczynski <qris70@gmail.com>
2018-05-24 19:28:48 +0200

    Nowe testy

--- Test1/Books/src/main/java/Library.java
+++ Test1/Books/src/main/java/Library.java
@@ -1,3 +1,5 @@
+
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -5,11 +7,11 @@ import java.util.stream.Collectors;
 public class Library {
     private List<Book> books = new ArrayList<>();
 
-    public static boolean filterOutAnteks(Book book) {
-        return !book.getAuthor().equals("Antek");
-    }
 
     public Library(List<Book> books) {
+        if (books.isEmpty()) {
+            throw new IllegalArgumentException("Book list cannot not be empty!");
+        }
         this.books.addAll(books);
     }
 
@@ -17,8 +19,21 @@ public class Library {
         return books.stream().map(Book::getAuthor).collect(Collectors.toList());
     }
 
-    public List<Book> getNotAnteks() {
-        return books.stream().filter(Library::filterOutAnteks).collect(Collectors.toList());
+    public List<String> getTitles() {
+        return books.stream().map(Book::getTitle).collect(Collectors.toList());
     }
+
+    public List<Integer> getPrices() {
+        return books.stream().map(Book::getPrice).collect(Collectors.toList());
+    }
+
+    public int getTotalPrice() {
+        return books.stream().map(Book::getPrice).reduce(0, (sum, price) -> sum + price);
+    }
+
+    public String getFormattedAuthors() {
+        return books.stream().map(Book::getAuthor).reduce("", (raport, author) -> raport + "- " + author + "\n");
+    }
+
 
 }
