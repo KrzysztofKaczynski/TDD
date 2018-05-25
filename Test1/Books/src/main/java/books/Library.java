6aea6ed99f92e4530f10b8563f09d0322b75519f
Krzysztof Kaczyński <qris70@gmail.com>
2018-05-25 10:29:29 +0200

    Plus MoneyService

--- Test1/Books/src/main/java/books/Library.java
+++ Test1/Books/src/main/java/books/Library.java
@@ -1,40 +1,86 @@
 package books;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Library {
-    private List<Book> books = new ArrayList<>();
 
+    private List<Book> books = new ArrayList<>();
+    MoneyLaunderingService moneyLaunderingService;
 
     public Library(List<Book> books) {
-        if (books.isEmpty()) {
-            throw new IllegalArgumentException("books.Book list cannot be empty");
-        }
         this.books.addAll(books);
     }
 
-    public List<String> getAuthors() {
-        return books.stream().map(Book::getAuthor).collect(Collectors.toList());
+    public Library(List<Book> books, MoneyLaunderingService moneyLaunderingService) {
+        this.books = books;
+        this.moneyLaunderingService = moneyLaunderingService;
     }
 
-    public List<String> getTitles() {
-        return books.stream().map(Book::getTitle).collect(Collectors.toList());
+    public Set<String> getAuthors() {
+        return books.stream().map(book -> book.getAuthor()).collect(Collectors.toSet());
     }
 
-    public List<Integer> getPrices() {
-        return books.stream().map(Book::getPrice).collect(Collectors.toList());
+    public List<String> getTitle() {
+        return books.stream().map(book -> book.getTitle()).collect(Collectors.toList());
     }
 
-    public int getTotalPrices() {
-        return books.stream().map(Book::getPrice).reduce(0, (sum, price) -> sum + price);
+    public List<Integer> getPrice() {
+        return books.stream().map(book -> book.getPrice()).collect(Collectors.toList());
     }
 
-    public String getFormattedAuthors() {
-        return books.stream().map(Book::getAuthor).reduce("", (raport, author) -> raport + "- " + author + "\n");
+    public List<String> getBooksOFAuthor(String author) {
+        return books.stream().filter(book -> book.getAuthor().equals(author)).map(book -> book.getTitle()).collect(Collectors.toList());
     }
 
+    public List<Book> getBooksByTitle(String title) {
+        return books.stream().filter(book -> book.getTitle().equals(title)).collect(Collectors.toList());
+    }
+
+    public List<Book> getMyPrice(Integer price) {
+        return books.stream().filter(book -> book.getPrice().equals(price)).collect(Collectors.toList());
+    }
+
+    public List<Book> getBooksByRangePrice(Integer min, Integer max) {
+        return books.stream().filter(book -> book.getPrice() > min).filter(book -> book.getPrice() < max).collect(Collectors.toList());
+    }
+
+    public Integer getReduce() {
+        return books.stream().map(book -> book.getPrice()).reduce(0, (price1, price2) -> price1 + price2);
+    }
+
+    //- zwrócenie sumy cen wszystkich książek podanego autora, wyłączając podany tytuł
+    public Integer getPriceAllBooksWithoutTitle(String author, String title) {
+        return books.stream().
+                filter(book -> !book.getTitle().equals(title)).
+                filter(book -> book.getAuthor().equals(author)).
+                map(book -> book.getPrice()).reduce(0, (price1, price2) -> price1 + price2);
+    }
+
+
+    //zwrocenie liczby ksiazek wydanych w danycm roku
+    public List<Book> getBooksByDate(int year) {
+        return books.stream().filter(book -> book.getDate().getYear() == year).collect(Collectors.toList());
+    }
+
+    //wydanie listy ksiazek danego autora wydaych w danym roku o cenie nie wyzszej niz podana
+    public List<Book> getListOfBooksOfAuthorInYearWithPriceHigher(String author, Integer price, int year) {
+        return books.stream().
+                filter(book -> book.getAuthor().equals(author)).
+                filter(book -> book.getDate().getYear() == year).
+                filter(book -> book.getPrice() > price).collect(Collectors.toList());
+    }
+
+    public Integer getPriceByAuthorAndTitle(String author, String title) {
+        Integer result = books.stream().filter(book -> book.getAuthor().equals(author)).
+                filter(book -> book.getTitle().equals(title))
+                .map(book -> book.getPrice()).reduce(0, (price, price2) -> price + price2);
+        return moneyLaunderingService.increasePrice(result);
+    }
 
 
 }
---
+++ Test1/Books/src/main/java/books/MoneyLaunderingService.java
@@ -0,0 +1,5 @@
+package books;
+
+public interface MoneyLaunderingService {
+    Integer increasePrice(Integer price);
+}
